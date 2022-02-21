a0fbc1dab86897b07a5f84fd254cd2287f15176c
Persifer <giorgino.antonio32@protonmail.com>
2022-02-21 09:48:50 +0100

    First commi

---
+++ src/test/java/com/virgo/backend/VirgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virgo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
