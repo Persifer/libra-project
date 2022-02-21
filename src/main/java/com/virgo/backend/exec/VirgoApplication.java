991065f7b1829c670c170e87d84ca6bcd06347d2
Persifer <giorgino.antonio32@protonmail.com>
2022-02-21 17:13:27 +0100

    Adding the tag to enable the repository in Spring

--- src/main/java/com/virgo/backend/exec/VirgoApplication.java
+++ src/main/java/com/virgo/backend/exec/VirgoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 //(scanBasePackages = {"com.virgo.backend.service","com.virgo.backend.controller"})
-//@EnableJpaRepositories(basePackages = {"com.virgo.backend.repository"} )
+@EnableJpaRepositories(basePackages = {"com.virgo.backend.repository"} )
 @EntityScan( basePackages = {"com.virgo.backend.model"} )
 public class VirgoApplication {
 
