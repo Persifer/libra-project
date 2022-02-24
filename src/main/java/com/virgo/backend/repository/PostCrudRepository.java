5f2bc29b3a98ce7dcc2501fbc90a09b6d3c9352a
Persifer <giorgino.antonio32@protonmail.com>
2022-02-24 16:35:33 +0100

    minor changes

--- src/main/java/com/virgo/backend/repository/PostCrudRepository.java
+++ src/main/java/com/virgo/backend/repository/PostCrudRepository.java
@@ -36,10 +36,10 @@ public interface PostCrudRepository extends CrudRepository<Post, Integer> {
     )
     public List<Post> getPostWithWords(String parola);
 
-    // Prende tutti i post che 
+
     @Query(
             value = "SELECT * FROM Post p JOIN Utente u ON (u.id_utente = p.id_utente)" +
-                    "WHERE u.id_utente = ?1 p.descrizione LIKE '%?2%' OR p.titolo LIKE '%?2%' ",
+                    "WHERE u.id_utente = ?1 AND p.descrizione LIKE '%?2%' OR p.titolo LIKE '%?2%' ",
             nativeQuery = true
     )
     public List<Post> getPostWithWordsForUser(Integer userId, String parola);
