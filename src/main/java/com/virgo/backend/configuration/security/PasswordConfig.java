9b0baa7d2222efc385930f322d7ec017d15458a9
Persifer <giorgino.antonio32@protonmail.com>
2022-03-15 22:01:34 +0100

    Added the file that handle the password encode

---
+++ src/main/java/com/virgo/backend/configuration/security/PasswordConfig.java
@@ -0,0 +1,12 @@
+package com.virgo.backend.configuration.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+    public PasswordEncoder passwordEncoder(){
+        
+    }
+}
