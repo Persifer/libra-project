ced5955afdbc0a3898db7ab868b4e11244a84a35
Persifer <giorgino.antonio32@protonmail.com>
2022-03-15 22:00:15 +0100

    Added a first way to create a default user

--- src/main/java/com/virgo/backend/configuration/security/LibraSecurityConfiguration.java
+++ src/main/java/com/virgo/backend/configuration/security/LibraSecurityConfiguration.java
@@ -1,9 +1,15 @@
 package com.virgo.backend.configuration.security;
 
+import com.virgo.backend.utils.Constants;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -15,10 +21,25 @@ public class LibraSecurityConfiguration extends WebSecurityConfigurerAdapter {
         //super.configure(http);
         http
                 .authorizeHttpRequests() // vogliamo richieste autorizzate
+                .antMatchers("/","/index.hmtl") // permette di dire a spring security quali sono gli url su cui non deve richiedere username e password. Gli url whitelistati
+                .permitAll()
                 .anyRequest() // ogni richiesta deve essere autenticata
                 .authenticated() // qui diciamo che ogni richiesta deve essere autenticata
                 .and()
                 .httpBasic(); //vogliamo che il meccanismo che forzi l'autenticazione del client sia quello base
 
+    }
+
+    @Override
+    @Bean
+    // È il metodo da cui si preleva l'user dal database
+    protected UserDetailsService userDetailsService() {
+
+        UserDetails antonioUser = User.builder()
+                .username("Antonio")
+                .password("password")
+                .roles(Constants.USEROLE).build();
+
+        return new InMemoryUserDetailsManager(antonioUser);
     }
 }
